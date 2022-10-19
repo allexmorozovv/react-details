6a1ae2eb0586f591ae7c7e5ecd4a5389cef0e140
allexmorozovv <allexmorozovv@gmail.com>
2022-10-19 18:57:01 +0300

    add Accordion and Rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from "./components/Accordion";
+import {Rating} from "./components/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+
+            <Rating value={0}/>
+            <Rating value={0}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+            <Accordion title={'Hey'} collapsed={true}/>
+            <Accordion title={'Yo'} collapsed={false}/>
+        </div>
+    );
 }
 
 export default App;

43f810bcfc95018c1cf62051b0e3f2beb23776b2
allexmorozovv <allexmorozovv@gmail.com>
2022-11-09 17:35:30 +0300

    refactor UncontrolledAccordion.tsx

--- src/components/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion.tsx
@@ -6,22 +6,20 @@ type AccordionPropsType = {
 
 export const UncontrolledAccordion = (props: AccordionPropsType) => {
 
-    let [collapsed, setCollapsed] = useState(false)
+    let [collapsed, setCollapsed] = useState(true)
 
     return <div>
-        <AccordionTitle title={props.title}/>
-        <button onClick={() => {
-            setCollapsed(!collapsed)}}>TOGGLE</button>
+        <AccordionTitle title={props.title} onClick={() => {setCollapsed(!collapsed)}}/>
         {!collapsed && <AccordionBody/>}
     </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
+    onClick: () => void
 }
 const AccordionTitle = (props: AccordionTitlePropsType) => {
-
-    return <h3>{props.title}</h3>
+    return <h3 onClick={()=>{props.onClick()}}>{props.title}</h3>
 }
 const AccordionBody = () => {
     return <div>
